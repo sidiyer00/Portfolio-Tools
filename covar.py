e1f4cb3f157f7d540e1e2d0fa1006f8efa7c4464
Siddharth Iyer <sidiyer612@gmail.com>
2020-11-30 12:22:53 -0500

    Removed irrelevant files

--- covar.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import plotly.express as px
-
-
-df = pd.read_excel("C:/Users/sidiy/Documents/Projects/Quant/Portfolio Tools/subset1.xlsx")
-print(df)
-
-fig = px.scatter(df)
-fig.show()
